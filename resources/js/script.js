6aa58eb24ae712d527def5cdaa8e03c9bf920354
sequeirajonathan <sequeira.s.jonathan@gmail.com>
2018-11-28 21:19:43 -0500

    Added Sound to script

--- resources/js/script.js
+++ resources/js/script.js
@@ -18,6 +18,19 @@ let gifs = {
 
 let scratches = 0;
 
+let audio1 = new Audio("resources/sound/win.ogg");
+let audio2 = new Audio("resources/sound/win.wav");
+let audio3 = new Audio("resources/sound/win.ogg");
+let audio4 = new Audio("resources/sound/win.wav");
+let audio5 = new Audio("resources/sound/win.ogg");
+let audio6 = new Audio("resources/sound/win.wav");
+let audio7 = new Audio("resources/sound/win.ogg");
+let audio8 = new Audio("resources/sound/win.wav");
+let audio9 = new Audio("resources/sound/win.ogg");
+let audio10 = new Audio("resources/sound/win.wav");
+let audio11 = new Audio("resources/sound/win.ogg");
+let audio12 = new Audio("resources/sound/win.wav");
+
 function showNumber1() {
   buttons.solid1.style.display = "none";
   scratches++;
@@ -72,30 +85,42 @@ function openColorBox() {
 
 buttons.solid1.addEventListener("click", function() {
   gifs.scratch1.style.zIndex = "1";
+  audio1.play();
+  audio2.play();
   setTimeout(showNumber1, 2250);
 });
 
 buttons.solid2.addEventListener("click", function() {
   gifs.scratch2.style.zIndex = "1";
+  audio3.play();
+  audio4.play();
   setTimeout(showNumber2, 2250);
 });
 
 buttons.solid3.addEventListener("click", function() {
   gifs.scratch3.style.zIndex = "1";
+  audio5.play();
+  audio6.play();
   setTimeout(showNumber3, 2250);
 });
 
 buttons.solid4.addEventListener("click", function() {
   gifs.scratch4.style.zIndex = "1";
+  audio7.play();
+  audio8.play();
   setTimeout(showNumber4, 2250);
 });
 
 buttons.solid5.addEventListener("click", function() {
   gifs.scratch5.style.zIndex = "1";
+  audio9.play();
+  audio10.play();
   setTimeout(showNumber5, 2250);
 });
 
 buttons.solid6.addEventListener("click", function() {
   gifs.scratch6.style.zIndex = "1";
+  audio11.play();
+  audio12.play();
   setTimeout(showNumber6, 2250);
 });
